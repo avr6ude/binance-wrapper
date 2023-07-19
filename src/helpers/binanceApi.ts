0bceb782999641e01012eb03d7ebf8151326f157
avr6ude <canuckchan@gmail.com>
2023-07-19 11:46:13 +0200

    update api endpoint

--- src/helpers/binanceApi.ts
+++ src/helpers/binanceApi.ts
@@ -4,9 +4,7 @@ const BASE_URL = 'https://api.binance.com'
 
 export default async function getTickerData(symbol: string) {
   try {
-    const res = await axios.get(
-      `${BASE_URL}/api/v3/ticker/24hr?symbol=${symbol}`
-    )
+    const res = await axios.get(`${BASE_URL}/api/v3/depth?symbol=${symbol}`)
     return res.data
   } catch (e) {
     console.error('Error getting ticket data', e)
