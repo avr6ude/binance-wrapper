1cc00ffdf0050231f1d4895c895defeca6365089
avr6ude <canuckchan@gmail.com>
2023-07-19 19:35:30 +0200

    fetch all the pairs with usdt and exclude some

--- src/helpers/binanceApi.ts
+++ src/helpers/binanceApi.ts
@@ -2,6 +2,12 @@ import axios from 'axios'
 
 const BASE_URL = 'https://api.binance.com'
 
+interface BinanceSymbol {
+  symbol: string
+  quoteAsset: string
+  baseAsset: string
+}
+
 export default async function getBinanceData(symbol: string, endpoint: string) {
   try {
     const res = await axios.get(
@@ -11,5 +17,17 @@ export default async function getBinanceData(symbol: string, endpoint: string) {
   } catch (e) {
     console.error('Error getting ticket data', e)
     throw e
+  }
+}
+
+export async function getUSDTPairs() {
+  try {
+    const res = await axios.get(`${BASE_URL}/api/v3/exchangeInfo`)
+    const symbols: BinanceSymbol[] = res.data.symbols
+    const USDTpairs = symbols.filter((s) => s.quoteAsset === 'USDT')
+    return USDTpairs
+  } catch (e) {
+    console.error('Error getting ticket data', e)
+    return []
   }
 }
