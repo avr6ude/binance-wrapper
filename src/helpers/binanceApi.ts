b8944d79d774507cdba2e36be2ac82485ad19ecd
avr6ude <canuckchan@gmail.com>
2023-07-19 12:30:38 +0200

    update api call method to accept diff endpoints

--- src/helpers/binanceApi.ts
+++ src/helpers/binanceApi.ts
@@ -2,9 +2,11 @@ import axios from 'axios'
 
 const BASE_URL = 'https://api.binance.com'
 
-export default async function getTickerData(symbol: string) {
+export default async function getBinanceData(symbol: string, endpoint: string) {
   try {
-    const res = await axios.get(`${BASE_URL}/api/v3/depth?symbol=${symbol}`)
+    const res = await axios.get(
+      `${BASE_URL}/api/v3/${endpoint}?symbol=${symbol}`
+    )
     return res.data
   } catch (e) {
     console.error('Error getting ticket data', e)
