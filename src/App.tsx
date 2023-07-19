fa1f6c577675c5ecfc29af25e3862d1548c6fdc4
avr6ude <canuckchan@gmail.com>
2023-07-19 19:36:05 +0200

    add cycled ticker checker

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Suspense, useEffect, useState } from 'react'
-import getBinanceData from 'helpers/binanceApi'
+import { Suspense, useEffect, useRef, useState } from 'react'
+import excludedPairs from 'data/excludedPairs'
+import getBinanceData, { getUSDTPairs } from 'helpers/binanceApi'
 
 interface OrderBook {
   bids: string[][]
@@ -8,22 +9,34 @@ interface OrderBook {
 
 export default function () {
   const [tickerData, setTickerData] = useState<OrderBook | null>(null)
-  const [alert, setAlert] = useState('')
+  const [alerts, setAlerts] = useState<string[]>([])
   const [currentPrice, setCurrentPrice] = useState(0)
-
-  const ticker = 'BTCUSDT'
+  const [filteredPairs, setFilteredPairs] = useState<string[]>([])
+  const tickerIndex = useRef(0)
 
   useEffect(() => {
-    const fetchPrice = async () => {
+    const fetchPairs = async () => {
+      const USDTPairs = await getUSDTPairs()
+      const pairs = USDTPairs.map((pair) => pair.symbol)
+      const filtered = pairs.filter((pair) => !excludedPairs.includes(pair))
+      setFilteredPairs(filtered)
+    }
+
+    void fetchPairs()
+  }, [])
+
+  useEffect(() => {
+    const fetchPrice = async (ticker: string): Promise<OrderBook> => {
       try {
         const data = await getBinanceData(ticker, 'depth')
-        setTickerData(data)
+        return data || { bids: [], asks: [] }
       } catch (e) {
         console.error(`Error fetching price for ${ticker}: ${e}`)
+        return { bids: [], asks: [] }
       }
     }
 
-    const fetchTradeData = async () => {
+    const fetchTradeData = async (ticker: string) => {
       try {
         const trades = await getBinanceData(ticker, 'trades')
         if (trades && trades.length > 0) {
@@ -32,9 +45,14 @@ export default function () {
       } catch (e) {
         console.error(`Error fetching trade data for ${ticker}: ${e}`)
       }
+      return 0
     }
 
-    const checkDifference = () => {
+    const checkDifference = (
+      ticker: string,
+      tickerData: OrderBook,
+      currentPrice: number
+    ) => {
       if (!tickerData || currentPrice === 0) return
 
       const filteredBids = tickerData.bids.filter(
@@ -58,30 +76,43 @@ export default function () {
         Math.abs(totalBids - totalAsks) / ((totalBids + totalAsks) / 2)
 
       if (difference > 0.5) {
-        setAlert(`${Math.round(difference * 100)}% | ${ticker}`)
-      } else if (difference <= 0.5) {
-        setAlert('')
+        const newAlert = `${Math.round(difference * 100)}% | ${ticker}`
+        setAlerts((prevAlerts) =>
+          Array.from(new Set([...prevAlerts, newAlert]))
+        )
       }
     }
 
-    void fetchPrice()
-    void fetchTradeData()
-    checkDifference()
+    const fetchTickerData = async () => {
+      const ticker = filteredPairs[tickerIndex.current]
+      if (ticker) {
+        const [data, price] = await Promise.all([
+          fetchPrice(ticker),
+          fetchTradeData(ticker),
+        ])
+        setTickerData(data)
+        setCurrentPrice(price)
+        checkDifference(ticker, data, price)
+        tickerIndex.current = (tickerIndex.current + 1) % filteredPairs.length
+      }
+    }
 
-    const intervalId = setInterval(() => {
-      void fetchPrice()
-      void fetchTradeData()
-      checkDifference()
-    }, 10000)
+    void fetchTickerData()
+
+    const intervalId = setInterval(fetchTickerData, 5000)
 
     return () => clearInterval(intervalId)
-  }, [currentPrice, tickerData])
+  }, [currentPrice, filteredPairs, tickerData])
 
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <div className="container mx-auto max-w-prose p-10 prose">
         <h1>Ticker Data</h1>
-        {alert && <p>{alert}</p>}
+        {alerts.length > 0 ? (
+          alerts.map((alert) => <p>{alert}</p>)
+        ) : (
+          <p>No alerts</p>
+        )}
       </div>
     </Suspense>
   )
